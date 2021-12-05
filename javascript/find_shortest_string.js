2c9805f14793ad35ae05259c5ecdf0af3bd06ef7
Michael Lobman <michael.lobman@gmail.com>
2021-12-05 16:21:02 -0500

    Complete assignment

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,16 @@
 function findShortestString(arr) {
-  // type your code here
+
+  let shortest = arr[0]
+
+  for (const string of arr){
+
+    if (string.length < shortest.length) {
+
+      shortest = string;
+    }
+  }
+
+  return shortest;
 }
 
 if (require.main === module) {
@@ -12,15 +23,67 @@ if (require.main === module) {
   console.log("Expecting: 'hi'");
   console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
 
+  console.log("Expecting: 'I'");
+  console.log("=>", findShortestString(['Hi', 'I', 'am', 'Mike']));
+
   console.log("");
 
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+
+  const startTime = Date.now();
+
+  let shortCount = 0;
+  let longCount = 0;
+
+  while (shortCount < 1000) {
+    const shortInput = [];
+
+    for (let i = 0; i < 5; ++i) {
+      shortInput.push("hello")
+    }
+
+    findShortestString(shortInput)
+    ++shortCount
+  }
+  while (longCount < 1000) {
+    const longInput = [];
+
+    for (let i = 0; i < 100; ++i) {
+      longInput.push("goodbye")
+    }
+
+    findShortestString(longInput);
+    ++longCount
+
+  }
+
+  const stopTime = Date.now();
+
+  const avgTime = (stopTime - startTime) / 2000;
+
+  console.log(`Average runtime for findShortestString() = ${avgTime} seconds`)
+
+
+
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+
+given an array of strings, return the shortest string
+in the array... if two arrays are the same length,
+return the first one to appear.
+
+this needs to iterate through an array, checking each
+string to see which is longest. the current longest
+should be stored into a variable, and that will now
+be the length against which is checked as the function
+iterates through the arr
+*/
